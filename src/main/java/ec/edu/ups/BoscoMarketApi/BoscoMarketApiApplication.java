e697ca1816532050392e4fef925cce5641995adf
Ivàn Sànchez <isanchezg1@est.ups.edu.ec>
2022-06-28 15:01:42 -0500

    Initial commit

---
+++ src/main/java/ec/edu/ups/BoscoMarketApi/BoscoMarketApiApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.BoscoMarketApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoscoMarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoscoMarketApiApplication.class, args);
+	}
+
+}
