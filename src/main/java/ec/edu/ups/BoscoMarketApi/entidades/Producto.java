5f7fbaa580755289b87dbd1ac13a803c1b4ab6fc
Ivàn Sànchez <isanchezg1@est.ups.edu.ec>
2022-06-28 17:38:22 -0500

    Relación Producto-Sucursal

---
+++ src/main/java/ec/edu/ups/BoscoMarketApi/entidades/Producto.java
@@ -0,0 +1,29 @@
+package ec.edu.ups.BoscoMarketApi.entidades;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Producto implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
+    private Long id;
+    @Getter @Setter
+    private String nombre;
+    @Getter @Setter
+    private double precio;
+    @Getter @Setter
+    private int stock;
+    @Getter @Setter
+    private String descripcion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Sucursal sucursal;
+
+    public Producto() {
+    }
+}
