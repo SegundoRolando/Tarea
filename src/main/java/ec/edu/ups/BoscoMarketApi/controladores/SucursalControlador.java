e8bcd04f6017376c08b0ff554db434a446cb2541
JoseTapiaV <joseluis01.25.1998@gmail.com>
2022-06-29 15:29:13 -0500

    Sucursal terminado y funcionando

---
+++ src/main/java/ec/edu/ups/BoscoMarketApi/controladores/SucursalControlador.java
@@ -0,0 +1,31 @@
+package ec.edu.ups.BoscoMarketApi.controladores;
+
+import ec.edu.ups.BoscoMarketApi.entidades.Sucursal;
+import ec.edu.ups.BoscoMarketApi.servicios.SucursalServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class SucursalControlador {
+
+    private SucursalServicio sucursalServicio;
+
+    @Autowired
+    public void setSucursalServicio(SucursalServicio sucursalServicio) {
+        this.sucursalServicio = sucursalServicio;
+    }
+
+
+    @GetMapping("/sucursales")
+    public ResponseEntity<List<Sucursal>> getAllSucursales(){
+        List<Sucursal> sucursalList = sucursalServicio.findAll();
+        return  new ResponseEntity<List<Sucursal>>(sucursalList, HttpStatus.OK);
+    }
+
+
+}
--- src/main/java/ec/edu/ups/BoscoMarketApi/servicios/SucursalServicio.java
+++ src/main/java/ec/edu/ups/BoscoMarketApi/servicios/SucursalServicio.java
@@ -5,6 +5,8 @@ import ec.edu.ups.BoscoMarketApi.repositorios.SucursalRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class SucursalServicio {
     @Autowired
@@ -12,6 +14,10 @@ public class SucursalServicio {
 
     public Sucursal findById(Long id) {
         return sucursalRepositorio.findById(id).orElse(null);
+    }
+
+    public List<Sucursal> findAll(){
+        return (List<Sucursal>) sucursalRepositorio.findAll();
     }
 
 }
